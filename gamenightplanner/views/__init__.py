6353759dd420bd5dd3e7ec7803d5fef074235dc8
Tomi Leppänen <tomi.leppanen@telemail.fi>
2017-03-30 23:16:36 +0300

    views: Added CreateViewWithInlines
    Added CreateViewWithInlines to support formsets in CreateViews.

--- gamenightplanner/views/__init__.py
+++ gamenightplanner/views/__init__.py
@@ -42,6 +42,50 @@ class CreateWithAddedInfoMixin(CreateView):
         form.instance.added = now()
         return super().form_valid(form)
 
+class CreateViewWithInlines(CreateView):
+    inlines = {}
+
+    def get_context_data(self, **kwargs):
+        for formset in self.inlines.keys():
+            if formset not in kwargs:
+                formset_kwargs = {}
+                if self.request.method in ('POST', 'PUT'):
+                    formset_kwargs['data'] = self.request.POST
+                if self.object:
+                    formset_kwargs['instance'] = self.object
+                kwargs[formset] = self.inlines[formset](**formset_kwargs)
+        return super().get_context_data(**kwargs)
+
+    def all_valid(self, form, **formsets):
+        self.object = form.save()
+        for name, formset in formsets.items():
+            formset.instance = self.object
+            formset.save()
+        return self.form_valid(form)
+
+    def all_invalid(self, form, **formsets):
+        return self.render_to_response(self.get_context_data(form=form,
+                                                             **formsets))
+
+    def post(self, request, *args, **kwargs):
+        self.object = None
+        invalid = False
+
+        form = self.get_form()
+        if not form.is_valid():
+            invalid = True
+
+        formsets = {}
+        for name, formset in self.inlines.items():
+            formsets[name] = formset(request.POST)
+            if not formsets[name].is_valid():
+                invalid = True
+
+        if invalid:
+            return self.all_invalid(form, **formsets)
+        else:
+            return self.all_valid(form, **formsets)
+
 class MainView(TemplateView):
     template_name = 'gamenightplanner/main.html'
 
